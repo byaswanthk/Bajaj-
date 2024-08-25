485aae0ec85d77356ae43b20111a4a89ed01edf5
Yaswanth Kumar <93034207+byaswanthk@users.noreply.github.com>
2024-08-25 12:10:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app
+  .route("/bfhl")
+  .get((req, res) => {
+    res.status(200).json({ operation_code: 1 });
+  })
+  .post((req, res) => {
+    const data = req.body.data || [];
+    const numbers = [];
+    const alphabets = [];
+    let highest_alphabet = "";
+
+    for (const item of data) {
+        if (typeof item === 'number') {
+          numbers.push(item);
+        } else if (typeof item === 'string' && item.length === 1 && isNaN(item)) {
+          alphabets.push(item);
+          if (!highest_alphabet || item.toUpperCase() > highest_alphabet.toUpperCase()) {
+            highest_alphabet = item;
+          }
+        }
+      }      
+
+    res.json({
+      is_success: true,
+      user_id: "yaswanth_01072004",
+      email: "yaswanth.kumar2021@vitstudent.ac.in",
+      roll_number: "21BCE5837",
+      numbers: numbers,
+      alphabets: alphabets,
+      highest_alphabet: highest_alphabet ? [highest_alphabet] : [],
+    });
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
